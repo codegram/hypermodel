4600a67201973ab79f1277439322d64e1bba73f3
Oriol Gual <oriol.gual@gmail.com>
2012-06-04 20:43:13 +0200

    Extract if check into method

--- lib/hypermodel/representation.rb
+++ lib/hypermodel/representation.rb
@@ -26,31 +26,28 @@ module Hypermodel
 
     def links
       @@links.inject({self: context.polymorphic_url(record)}) do |links, (name, options, block)|
-        if (options[:if] && options[:if].call(record, context)) || options[:if] == nil
-          value = if block
-            block.call(record, context)
-          else
-            context.polymorphic_url(record.send(name))
-          end
-          links.update(name => value)
+        next links if should_skip(options)
+
+        link = if block
+          block.call(record, context)
         else
-          links
+          context.polymorphic_url(record.send(name))
         end
+
+        links.update(name => link)
       end
     end
 
     def embedded
       @@embeds.inject({}) do |embedded, (name, options, block)|
-        if (options[:if] && options[:if].call(record, context)) || options[:if] == nil
-          value = if block
-            block.call(record, context)
-          else
-            record.send(name)
-          end
-          embedded.update(name => value)
+        next embedded if should_skip(options)
+
+        value = if block
+          block.call(record, context)
         else
-          embedded
+          record.send(name)
         end
+        embedded.update(name => value)
       end
     end
 
@@ -58,6 +55,14 @@ module Hypermodel
       record.attributes.select do |attribute, value|
         @@selected_attributes.include? attribute
       end
+    end
+
+    private
+    def should_skip(options)
+      return false unless options.include? :if
+      return false if options[:if] == nil
+
+      !options[:if].call(record, context)
     end
   end
 end
